95f2865e8ff0014f40fc5dc404e6869776dfca37
Luis Eduardo Soares <luisedu.soares@outlook.com>
2025-08-13 18:21:33 -0300

    feature(frontend): CI/CD pipelines for frontend project

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -19,7 +19,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
-        "warn",
+        "error",
         {
           vars: "all",
           varsIgnorePattern: "^_",
