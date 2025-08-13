c827e191ab4b0a5031a7b4a96936e2261f0df755
Luis Eduardo Soares <luisedu.soares@outlook.com>
2025-08-12 22:20:45 -0300

    feature(frontend): boilerplate setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,15 @@
-import Image from "next/image";
+import { Navbar } from "@/components/layout/navbar";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="min-h-screen transition-colors duration-300 bg-gradient-to-br from-blue-50 to-indigo-100">
+      <Navbar />
+
+      <div className="pt-24 px-6 pb-6 h-screen overflow-hidden">
+        <div className="h-full overflow-x-auto overflow-y-hidden max-w-7xl mx-auto">
+          boards here
+        </div>
+      </div>
     </div>
   );
 }
